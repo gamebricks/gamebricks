78d3276e97bad0fa5e91ab971bccce5824324f6f
Johannes Stein <johannesstein@freeze-dev.com>
2013-12-24 17:01:00 +0100

    Added generated files

---
+++ dist/gameboard.js
@@ -0,0 +1,16 @@
+udefine('gameboard/input', ['eventmap', 'mixedice'], function(EventMap, mixedice) {
+  
+  
+  
+});
+udefine('gameboard/loop', ['eventmap', 'mixedice'], function(EventMap, mixedice) {
+  
+  
+  
+});
+
+udefine('gameboard/preloader', ['eventmap', 'mixedice'], function(EventMap, mixedice) {
+  
+  
+  
+});
