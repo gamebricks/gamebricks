b9fe8f0bfa9265e0306d8272bfdb27948492f01d
Johannes Stein <johannesstein@freeze-dev.com>
2014-01-25 00:16:54 +0100

    Updated input module

--- src/input.js
+++ src/input.js
@@ -1,7 +1,17 @@
-udefine(['eventmap', 'mixedice'], function(EventMap, mixedice) {
-  
+udefine(['root', 'eventmap', 'gameboard/key'], function(root, EventMap, Key) {
+
   var Input = {};
-  
+
+  Input.define = Key.define;
+  Input.key = new EventMap();
+
+  root.addEventListener('keydown', function(evt) {
+    Input.key.trigger('down', evt.keyCode);
+  }, true);
+  root.addEventListener('keyup', function(evt) {
+    Input.key.trigger('up', evt.keyCode);
+  }, true);
+
   return Input;
-  
-});
+
+}); 
