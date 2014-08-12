cef4bf4eced0ba57af831c46f9dd8143e58d519e
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-12 15:00:57 +0200

    Added tests (not working yet)

---
+++ test/spec/input.js
@@ -0,0 +1,12 @@
+define('spec/input', ['gameboard/input'], function(Input) {
+  'use strict';
+  
+  describe('gameboard/input', function() {
+    
+    it('is an object', function() {
+      expect(Input).to.be.a('object');
+    });
+    
+  });
+  
+});
