315da71d3a715ff8190fd13dd11e41ebbd639468
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-12 14:59:32 +0200

    Tween stub

---
+++ src/tween.js
@@ -0,0 +1,16 @@
+define(function() {
+  
+  var Tween = (function() {
+    
+    var Tween = function() {
+      this.count = 1;
+    };
+    
+    Tween.prototype.animate = function(property, options) {
+      
+    };
+  })();
+  
+  return Tween;
+  
+});
