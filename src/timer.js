5611ef8dc7dfbf61175f6f6aba7dad95a35fba32
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-26 13:59:24 +0200

    Update timer logic

--- src/timer.js
+++ src/timer.js
@@ -1,9 +1,11 @@
 udefine(['mixedice', 'eventmap', 'performance'], function(mixedice, EventMap, performance) {
 
-  var Timer = function() {
+  var Timer = function(interval) {
     mixedice([this, Timer.prototype], new EventMap());
+    
+    var self = this;
 
-    this.interval = 1000;
+    this.interval = interval || 1000;
     this.startTime = -1;
 
     this.active = false;
@@ -12,14 +14,19 @@ udefine(['mixedice', 'eventmap', 'performance'], function(mixedice, EventMap, pe
     var oldTicks = 0;
 
     this.tick = function(currentTime) {
-      if (!this.active || !this.paused) {
+      if (!self.active || !self.paused) {
+        return;
+      }
+      
+      if (interval > 0) {
         return;
       }
 
-      this.trigger('tick');
+      self.trigger('tick');
 
-      if (currentTime - this.startTime) {
-        
+      if ((currentTime - self.startTime - self.interval) > oldTicks) {
+        oldTicks = currentTime;
+        self.trigger('interval');
       }
     };
   };
