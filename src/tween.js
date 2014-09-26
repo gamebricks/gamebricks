e46a51b08f4259b8ed0452a060b1cb3cd2d8fe8d
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-26 14:45:06 +0200

    Tweening prototype

--- src/tween.js
+++ src/tween.js
@@ -1,22 +1,34 @@
-udefine(function() {
-  
-  var Tween = (function() {
+udefine(['bezier-easing', 'gameboard/loop'], function(BezierEasing, Loop) {
+
+  var Tween = function() {
+    this.target = null;
+  };
+
+  Tween.prototype.animate = function(property, end, time, easing) {
+    var self = this;
     
-    var Tween = function() {
-      this.target = null;
-    };
-    
-    Tween.prototype.animate = function(property, end, time) {
-      if (this.target && typeof this.target[property] === 'number') {
-      	var start = this.target[property];
-      	
-      	var animateId = 'animate-' + Date.now();
-      	
-      	
-      }
-    };
-  })();
-  
+    if (this.target && typeof this.target[property] === 'number') {
+      var start = this.target[property];
+      
+      easing = easing || 'linear';
+
+      var timer = Loop.createTimer();
+      
+      timer.interval = time;
+      
+      timer.on('tick', function(ticks) {
+        var multiplicator = BezierEasing.css[easing](ticks / (timer.startTime + timer.interval));
+        
+        self.target[property] = (end - start) * multiplicator;
+      });
+      
+      timer.on('interval', function() {
+        timer.stop();
+      });
+
+    }
+  };
+
   return Tween;
-  
+
 });
