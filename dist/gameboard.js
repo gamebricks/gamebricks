1587ba1da16d439b21e164f4f0bc816a3925f344
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-09 01:31:03 +0100

    Release 0.2.0-alpha2

--- dist/gameboard.js
+++ dist/gameboard.js
@@ -265,6 +265,18 @@ udefine('gameboard/assetloader', ['root', 'eventmap', 'mixedice', './log'], func
 
 }));
 
+udefine('gameboard', ['gameboard/assetloader', 'gameboard/input', 'gameboard/loop', 'gameboard/log', 'gameboard/timer'], function(AssetLoader, Input, Loop, Log, Timer) {
+  
+  return {
+    AssetLoader: AssetLoader,
+    Input: Input,
+    Loop: Loop,
+    Log: Log,
+    Timer: Timer
+  };
+  
+});
+
 udefine('gameboard/input', ['root', 'eventmap', 'gameboard/key'], function(root, EventMap, Key) {
 
   var Input = {};
