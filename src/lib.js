3409e23f6b3924e721009ed6f71f897a8b2812e8
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:23:36 +0100

    Rename index to lib

---
+++ src/lib.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import AssetLoader from './assetloader';
+import Input from './input';
+import Loop from './loop';
+import Log from './log';
+import Timer from './timer';
+
+  
+export default {
+  AssetLoader: AssetLoader,
+  Input: Input,
+  Loop: Loop,
+  Log: Log,
+  Timer: Timer
+};
+  
