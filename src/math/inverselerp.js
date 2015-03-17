76f7365173e6be47bb941af537faeaa2c92947b8
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:24:36 +0100

    :fire: Update inverseLerp to ES6

--- src/math/inverselerp.js
+++ src/math/inverselerp.js
@@ -1,5 +1,9 @@
-udefine('inverselerp', ['lerp'], function(lerp) {
-	return function(min, max, amt) {
-		return max - lerp(min, max, amt);
-	};
-});
+'use strict';
+
+import lerp from './lerp';
+
+var inverseLerp = function(min, max, amt) {
+  return max - lerp(min, max, amt);
+};
+
+export default inverseLerp;
