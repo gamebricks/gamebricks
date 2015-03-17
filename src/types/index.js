3c7723890470f9824ac884a8df8ab5f69d689b81
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:32:33 +0100

    Add types shorthands

---
+++ src/types/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import Color from './color';
+import Vector2 from './vector2';
+import Vector3 from './vector3';
+import Rect from './rect';
+
+let Types = {};
+
+Types.Color = Color;
+Types.Vector2 = Vector2;
+Types.Vector3 = Vector3;
+Types.Rect = Rect;
+
+export default Types;
+export {Color, Vector2, Vector3, Rect};
