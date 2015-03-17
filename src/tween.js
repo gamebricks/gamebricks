e26f916df0a1eeee686df81954acfb0a781161e2
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:25:38 +0100

    :fire: Update Tween to ES6

--- src/tween.js
+++ src/tween.js
@@ -1,56 +1,58 @@
-udefine(['eventmap', './bezier-easing', './loop'], function(EventMap, BezierEasing, Loop) {
+'use strict';
 
-  var Tween = function() {
-    EventMap.mixin(this, Tween.prototype);
-    
+import EventMap from 'eventmap';
+import BezierEasing from './bezier-easing';
+import Loop from './loop';
+
+class Tween extends EventMap {
+  constructor() {
     this.target = null;
-  };
+  }
 
-  Tween.prototype.animate = function(property, end, time, easing) {
+  animate(property, end, time, easing) {
     var self = this;
-    
+
     if (this.target && typeof this.target[property] === 'number') {
       var start = this.target[property];
-      
+
       if (start === end) {
         this.trigger('start');
         this.trigger('end');
         return;
       }
-      
+
       easing = easing || 'linear';
 
       var timer = Loop.createTimer();
-      
+
       timer.interval = time;
-      
-      timer.on('start', function() {
+
+      timer.on('start', function () {
         self.trigger('start');
       });
-      
+
       timer.start();
-      
-      timer.on('tick', function(ticks) {
+
+      timer.on('tick', function (ticks) {
         var multiplicator = BezierEasing.css[easing](ticks / (timer.startTime + timer.interval));
         var points = (end - start) * multiplicator;
-        
+
         if (points > end) {
           points = end;
         }
-        
+
         self.target[property] = points;
         self.trigger('animate', points);
       });
-      
-      timer.on('interval', function() {
+
+      timer.on('interval', function () {
         timer.stop();
         self.target[property] = end;
         self.trigger('end');
       });
 
     }
-  };
+  }
+}
 
-  return Tween;
-
-});
+export default Tween;
