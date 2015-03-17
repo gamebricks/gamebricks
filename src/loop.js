6e0010890e1117ba4851abc7ff9e009b111f0789
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:23:58 +0100

    :fire: Update loop to ES6

--- src/loop.js
+++ src/loop.js
@@ -1,116 +1,116 @@
-udefine(['requestanimationframe', 'eventmap', 'performance', './timer'], function(requestAnimationFrame, EventMap, performance, Timer) {
-  
+'use strict';
+
+import requestAnimationFrame from 'requestanimationframe';
+import EventMap from 'eventmap';
+import performance from 'performance';
+import Timer from './timer';
+
+
+var loopEvents = new EventMap();
+var pausedEvents = {};
+var timers = [];
+
+/**
+ * @class Loop
+ * @static
+ */
+var Loop = (function () {
+
+  var isRunning = true;
+
   /**
-   * @module gameboard/loop
+   * @method run
    */
-  
-  'use strict';
-  
-  var loopEvents = new EventMap();
-  var pausedEvents = {};
-  var timers = [];
-  
-  /**
-   * @class Loop
-   * @static 
-   */
-  return (function() {
+  var run = function () {
+    var time;
 
-    var isRunning = true;
+    (function loop() {
+      requestAnimationFrame(loop);
 
-    /**
-     * @method run 
-     */
-    var run = function() {
-      var time;
+      var now = performance.now();
+      var dt = now - (time || now);
 
-      (function loop() {
-        requestAnimationFrame(loop);
+      time = now;
 
-        var now = performance.now();
-        var dt = now - (time || now);
-
-        time = now;
-
-        if (!isRunning) {
-          return;
-        }
-        
-        timers.forEach(function(timer) {
-          timer.tick(now);
-        });
-
-        var eventKeys = Object.keys(loopEvents.events.listeners);
-        
-        for (var i = 0, j = eventKeys.length; i < j; i++) {
-          (function(key) {
-            if (!pausedEvents[key]) {
-              loopEvents.trigger(key, dt);
-            }
-          })(eventKeys[i]);
-        }
-      })();
-    };
-
-    /**
-     * @method stop 
-     */
-    var stop = function() {
-      isRunning = false;
-    };
-
-    var clear = function() {
-      loopEvents.clear();
-      pausedEvents = {};
-    };
-    
-    var on = function(taskName, taskFunction) {
-      loopEvents.on(taskName, taskFunction);
-      pausedEvents[taskName] = false;
-    };
-    
-    var off = function(taskName) {
-      loopEvents.off(taskName);
-      if (pausedEvents[taskName] != null) {
-        delete pausedEvents[taskName];        
-      }
-    };
-
-    var pause = function(taskName) {
-      pausedEvents[taskName] = true;
-    };
-
-    var resume = function(taskName) {
-      if (taskName == null) {
-        isRunning = true;
+      if (!isRunning) {
         return;
       }
-      
-      pausedEvents[taskName] = false;
-    };
-    
-    var createTimer = function(interval) {
-      var timer = new Timer(interval);
-      timers.push(timer);
-      
-      return timer;
-    };
+
+      timers.forEach(function (timer) {
+        timer.tick(now);
+      });
+
+      var eventKeys = Object.keys(loopEvents.events.listeners);
+
+      for (var i = 0, j = eventKeys.length; i < j; i++) {
+        (function (key) {
+          if (!pausedEvents[key]) {
+            loopEvents.trigger(key, dt);
+          }
+        })(eventKeys[i]);
+      }
+    })();
+  };
+
+  /**
+   * @method stop
+   */
+  var stop = function () {
+    isRunning = false;
+  };
+
+  var clear = function () {
+    loopEvents.clear();
+    pausedEvents = {};
+  };
+
+  var on = function (taskName, taskFunction) {
+    loopEvents.on(taskName, taskFunction);
+    pausedEvents[taskName] = false;
+  };
+
+  var off = function (taskName) {
+    loopEvents.off(taskName);
+    if (pausedEvents[taskName] != null) {
+      delete pausedEvents[taskName];
+    }
+  };
+
+  var pause = function (taskName) {
+    pausedEvents[taskName] = true;
+  };
+
+  var resume = function (taskName) {
+    if (taskName == null) {
+      isRunning = true;
+      return;
+    }
+
+    pausedEvents[taskName] = false;
+  };
+
+  var createTimer = function (interval) {
+    var timer = new Timer(interval);
+    timers.push(timer);
+
+    return timer;
+  };
 
 
-    return {
-      run: run,
-      
-      stop: stop,
-      clear: clear,
-      
-      on: on,
-      off: off,
-      
-      pause: pause,
-      resume: resume,
-      
-      createTimer: createTimer
-    };
-  })();
-  
-});
+  return {
+    run: run,
+
+    stop: stop,
+    clear: clear,
+
+    on: on,
+    off: off,
+
+    pause: pause,
+    resume: resume,
+
+    createTimer: createTimer
+  };
+})();
+
+export default Loop;
