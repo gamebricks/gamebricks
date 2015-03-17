dc71d17220422abc07a093e3e014a1576a5b7db9
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:24:50 +0100

    :fire: Update lerp to ES6

--- src/math/lerp.js
+++ src/math/lerp.js
@@ -1,12 +1,16 @@
-udefine('lerp', ['clamp'], function(clamp) {
-	return function(min, max, amt) {
-    var tmpAmt = clamp(amt, 0.0, 1.0);
-    var diff = Math.abs(max - min);
-    
-    if (diff === 0) {
-    	return min;
-    } else {
-    	return min + (diff * tmpAmt);
-    }
-	};
-});
+'use strict';
+
+import clamp from './clamp';
+
+var lerp = function(min, max, amt) {
+  var tmpAmt = clamp(amt, 0.0, 1.0);
+  var diff = Math.abs(max - min);
+
+  if (diff === 0) {
+    return min;
+  } else {
+    return min + (diff * tmpAmt);
+  }
+};
+
+export default lerp;
