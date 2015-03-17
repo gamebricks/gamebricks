ee8cad43c0240527114f821d4d56b6ad44f7bd47
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-17 22:24:08 +0100

    :fire: Update clamp to ES6

--- src/math/clamp.js
+++ src/math/clamp.js
@@ -1,33 +1,32 @@
-udefine('clamp', function() {
-  'use strict';
-  
-  var clamp = function(value, min, max) {
-    var _ref, _ref1, _ref2;
-    if ( typeof value === 'object') {
-      _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
-    }
-    if (Array.isArray(min)) {
-      _ref1 = min, min = _ref1[0], max = _ref1[1];
-    }
-    if (min == null) {
-      min = 0.0;
-    }
-    if (max == null) {
-      max = 1.0;
-    }
-    if (min > max) {
-      _ref2 = [max, min], min = _ref2[0], max = _ref2[1];
-    }
-    if ((min <= value && value <= max)) {
-      return value;
-    } else {
-      if (value > max) {
-        return max;
-      } else {
-        return min;
-      }
-    }
-  };
+'use strict';
 
-  return clamp;
-});
+// TODO: Make this pretties and more ES6-like
+var clamp = function (value, min, max) {
+  var _ref, _ref1, _ref2;
+  if (typeof value === 'object') {
+    _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
+  }
+  if (Array.isArray(min)) {
+    _ref1 = min, min = _ref1[0], max = _ref1[1];
+  }
+  if (min == null) {
+    min = 0.0;
+  }
+  if (max == null) {
+    max = 1.0;
+  }
+  if (min > max) {
+    _ref2 = [max, min], min = _ref2[0], max = _ref2[1];
+  }
+  if ((min <= value && value <= max)) {
+    return value;
+  } else {
+    if (value > max) {
+      return max;
+    } else {
+      return min;
+    }
+  }
+};
+
+export default clamp;
