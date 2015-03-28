b1b94c597acf40350ca67cde376ce75303f4a74d
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-28 21:56:32 +0100

    Update test folder

---
+++ test/input.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import {Input} from 'gamebox';
+
+describe('gameboard/input', function () {
+
+  it('is an object', function () {
+    expect(Input).to.be.a('object');
+  });
+
+});
+  
