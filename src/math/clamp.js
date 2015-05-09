f7acc53de857330973a769bac65af1bb9f65b151
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-09 14:52:28 +0200

    :fire: Rewrote clamp module in ES6

--- src/math/clamp.js
+++ src/math/clamp.js
@@ -1,23 +1,10 @@
 'use strict';
 
-// TODO: Make this pretties and more ES6-like
-var clamp = function (value, min, max) {
-  var _ref, _ref1, _ref2;
-  if (typeof value === 'object') {
-    _ref = value, min = _ref.min, max = _ref.max, value = _ref.value;
-  }
-  if (Array.isArray(min)) {
-    _ref1 = min, min = _ref1[0], max = _ref1[1];
-  }
-  if (min == null) {
-    min = 0.0;
-  }
-  if (max == null) {
-    max = 1.0;
-  }
+var clamp = function(value, min = 0.0, max = 1.0) {
   if (min > max) {
-    _ref2 = [max, min], min = _ref2[0], max = _ref2[1];
+    [min, max] = [max, min];
   }
+
   if ((min <= value && value <= max)) {
     return value;
   } else {
@@ -27,6 +14,14 @@ var clamp = function (value, min, max) {
       return min;
     }
   }
+};
+
+clamp.array = function([value, min, max]) {
+  return clamp(value, min, max);
+};
+
+clamp.obj = function({value, min, max}) {
+  return clamp(value, min, max);
 };
 
 export default clamp;
